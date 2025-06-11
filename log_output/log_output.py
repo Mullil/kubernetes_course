d955cec5ec3cdf0fd237f2d51b0cd5b1917f7c34
Mullil <eetu.taatila@gmail.com>
2025-06-11 18:25:57 +0300

    Add exercise 1.7

--- log_output/log_output.py
+++ log_output/log_output.py
@@ -2,10 +2,28 @@ import time
 import random
 import string
 import datetime
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import uvicorn
 
+app = FastAPI()
+
+current_time = datetime.datetime.now(datetime.timezone.utc)
 random_str = ''.join(random.choices(string.ascii_lowercase, k=10))
 
-while True:
+
+@app.get("/")
+def index():
     current_time = datetime.datetime.now(datetime.timezone.utc)
-    print(f"{current_time}: {random_str}", flush=True)
-    time.sleep(5)
+    html = f'<html> <meta http-equiv="refresh" content="5"> <p>Time: {current_time}</p> <p>Random string: {random_str}</p> </html>'
+    return HTMLResponse(content=html)
+
+def update_loop():
+    global current_time
+    while True:
+        current_time = datetime.datetime.now(datetime.timezone.utc)
+        print(f"{current_time}: {random_str}", flush=True)
+        time.sleep(5)
+
+if __name__=="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=3000)
