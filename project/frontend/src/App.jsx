9eccd72a011e93708048da93558b7004f69d3290
Mullil <eetu.taatila@gmail.com>
2025-06-23 15:47:09 +0300

    Add exercise 1.13

--- project/frontend/src/App.jsx
+++ project/frontend/src/App.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodo] = useState([
+    "Learn Javascript",
+    "Learn React",
+    "Build a project"
+  ])
+
+
+  const todoList = () => {
+    return (
+      <ul>
+      {todos.map((todo, idx) =>
+        <li key={idx}>{todo}</li>
+      )}
+      </ul>
+    )
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const todo = event.target.todo.value
+    event.target.todo.value = ''
+    if (todo.length <= 140) setTodo(todos.concat(todo))
+  }
+
   return (
     <div>
-      TODO app
+      The project App
       <p><img style={{ height: 300, width: 300 }} src='/files/image' alt='Random'/></p>
+      <form onSubmit={ handleSubmit }>
+        <div><input name="todo"></input></div>
+        <button type="submit">Create todo</button>
+      </form>
+      {todoList()}
+
+      DevOps with Kubernetes 2025
     </div>
+    
   )
 }
 export default App;
