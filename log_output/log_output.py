7f1b0fb94dec5fab938cb4343500df62ce648a32
Mullil <eetu.taatila@gmail.com>
2025-06-25 21:34:19 +0300

    Add exercise 2.1

--- log_output/log_output.py
+++ log_output/log_output.py
@@ -2,21 +2,30 @@ import time
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 import uvicorn
+import requests
+import datetime
+import random
+import string
+import threading
 
+random_str = ''.join(random.choices(string.ascii_lowercase, k=10))
+current_time = datetime.datetime.now(datetime.timezone.utc)
 app = FastAPI()
 
 
 @app.get("/", response_class=PlainTextResponse)
 def index():
-    with open ("./files/logs.txt", "r") as file:
-        logs = file.read()
-    try:
-        with open ("./files/pong.txt", "r") as file:
-            pongs = file.read()
-    except:
-        pongs = 0
-    data = logs + "\n" + pongs
+    pongs = requests.get("http://ping-pong-svc:2345/pings").text
+    data = f"{current_time}: {random_str} \nPing / Pongs: {pongs}"
     return PlainTextResponse(content=data)
 
+def update_loop():
+    global current_time
+    while True:
+        current_time = datetime.datetime.now(datetime.timezone.utc)
+        print(f"{current_time}: {random_str}", flush=True)
+        time.sleep(5)
+
 if __name__=="__main__":
+    threading.Thread(target=update_loop, daemon=True).start()
     uvicorn.run(app, host="0.0.0.0", port=3000)
--- ping-pong-app/main.go
+++ ping-pong-app/main.go
@@ -4,39 +4,14 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 )
 
 var count int
-
-func readCountFromFile() int {
-	data, err := os.ReadFile("files/pong.txt")
-	if err != nil {
-		return 0
-	}
-	parts := strings.Split(string(data), ": ")
-	if len(parts) != 2 {
-		return 0
-	}
-	parsed, err := strconv.Atoi(strings.TrimSpace(parts[1]))
-	if err != nil {
-		return 0
-	}
-	return parsed
-}
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-	}
-
-	count = readCountFromFile()
-	initialData := fmt.Sprintf("Ping / Pongs: %d", count)
-	err := os.WriteFile("files/pong.txt", []byte(initialData), 0644)
-	if err != nil {
-		panic(err)
 	}
 
 	http.HandleFunc("/pingpong", func(w http.ResponseWriter, r *http.Request) {
@@ -47,6 +22,10 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+	})
+
+	http.HandleFunc("/pings", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "%d\n", count)
 	})
 
 	http.ListenAndServe(":"+port, nil)
