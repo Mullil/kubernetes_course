bf31af157c8853ba7bd9a47ed66d5c9aa23988ab
Mullil <eetu.taatila@gmail.com>
2025-08-21 13:04:13 +0300

    fix cronjob

---
+++ project/backup.sh
@@ -0,0 +1,7 @@
+set -e
+
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+FILE="backup-${DATE}.sql"
+
+PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -U $DB_USER $DB_NAME > $FILE
+gsutil cp $FILE gs://$GCS_BUCKET/backups/
