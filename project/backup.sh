0a948c6afbe3088f9639ba2219fcf6d33045a8ea
Mullil <eetu.taatila@gmail.com>
2025-08-23 11:11:35 +0300

    Delete incorrect backup cronjob

--- project/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-DATE=$(date +%Y-%m-%d_%H-%M-%S)
-FILE="backup-${DATE}.sql"
-
-PGPASSWORD=$DB_PASSWORD pg_dump -h $DB_HOST -U $DB_USER $DB_NAME > $FILE
-gsutil cp $FILE gs://$GCS_BUCKET/backups/
